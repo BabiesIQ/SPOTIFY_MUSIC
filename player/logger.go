f8aa4d5c71f32906f3c578a74b9d9ceb832bf77f
BabiesIQ.Tech <tridevk442@gmail.com>
2026-08-04 12:18:50 +0000

    python > go by @BabiesIQ.Tech

---
+++ player/logger.go
@@ -0,0 +1,41 @@
+/*
+ * SPOTIFY_MUSIC - Telegram Music Bot
+ *  Copyright (c) 2025-2026 BabiesIQ
+ *
+ *  Licensed under GNU GPL v3
+ *  See https://github.com/BabiesIQ/SPOTIFY_MUSIC
+ */
+
+package vc
+
+import (
+	"github.com/BabiesIQ/SPOTIFY_MUSIC/config"
+	"github.com/BabiesIQ/SPOTIFY_MUSIC/bot/utils"
+	"fmt"
+
+	td "github.com/BabiesIQ/gotdbot"
+)
+
+// sendLogger sends a formatted log message to the designated logger chat.
+// It includes details about the song being played, such as its title, duration, and the user who requested it.
+func sendLogger(client *td.Client, chatID int64, song *utils.CachedTrack) {
+	if chatID == 0 || song == nil || chatID == config.LoggerId {
+		return
+	}
+
+	text := fmt.Sprintf(
+		"<b>A song is playing</b> in <code>%d</code>\n\n‣ <b>Title:</b> <a href='%s'>%s</a>\n‣ <b>Duration:</b> %s\n‣ <b>Requested by:</b> %s\n‣ <b>Platform:</b> %s\n‣ <b>Is Video:</b> %t",
+		chatID,
+		song.URL,
+		song.Name,
+		utils.SecToMin(song.Duration),
+		song.User,
+		song.Platform,
+		song.IsVideo,
+	)
+
+	_, err := client.SendTextMessage(config.LoggerId, text, &td.SendTextMessageOpts{DisableWebPagePreview: true, ParseMode: "HTML"})
+	if err != nil {
+		logger.Warn("Failed to send the message", "error", err)
+	}
+}
