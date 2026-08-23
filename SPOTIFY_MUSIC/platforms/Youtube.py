df1384839eb56613f7e2d7cc449bcfedf8e4b290
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2026-08-23 05:35:12 +0530

    Replace youtubesearchpython with py_yt for video search
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- SPOTIFY_MUSIC/platforms/Youtube.py
+++ SPOTIFY_MUSIC/platforms/Youtube.py
@@ -38,7 +38,7 @@ import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython import VideosSearch
+from py_yt import VideosSearch
 from SPOTIFY_MUSIC.utils.database import is_on_off
 from SPOTIFY_MUSIC.utils.formatters import time_to_seconds
 import os
